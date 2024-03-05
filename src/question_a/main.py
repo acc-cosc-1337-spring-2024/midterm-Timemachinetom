43a3c33a3e59b0ac7da8e83d56e9e3236b22ec26
Timemachinetom <108629469+Timemachinetom@users.noreply.github.com>
2024-03-05 02:28:46 +0000

    Question a

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,6 @@
 #add import
+import question_a
+
+num = input("Enter a number")
+question_a.use_local_variable
+print(num)
--- tests/question_tests/question_tests.py
+++ tests/question_tests/question_tests.py
@@ -2,11 +2,12 @@
 import unittest
 
 #follow this example to add questions b, c, and d for testing including their functions
-from src.question_a.question_a import test_config
+from src.question_a.question_a import test_config, use_local_variable
 
 class Test_Config(unittest.TestCase):
 
     def test_question_a_config(self):
         self.assertEqual(True, test_config())
 
-
+    def test_use_local_variable(self):
+        self.assertEqual (100, use_local_variable(num))
